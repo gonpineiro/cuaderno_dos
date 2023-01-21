d9b2fcedc4ed32d563d13ad1dad56d4a00e55622
Gonzalo <gonzalo@estadodigital.com>
2023-01-21 19:36:39 -0300

    Configuracion basicas de JWT

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\ApiController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', [ApiController::class, 'login']);
+Route::post('register', [ApiController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::post('logout', [ApiController::class, 'logout']);
+    Route::post('refresh', [ApiController::class, 'refresh']);
+    Route::get('get_user', [ApiController::class, 'get_user']);
+
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{id}', [ProductController::class, 'show']);
+    Route::post('create', [ProductController::class, 'store']);
+    Route::put('update/{product}',  [ProductController::class, 'update']);
+    Route::delete('delete/{product}',  [ProductController::class, 'destroy']);
 });
