6121ac07148c40d018e8f753c0612be3e818318a
gpineiro <gon.pineiro@gmail.com>
2025-03-30 15:49:31 -0300

    Prueba adjunto de excel

---
+++ app/Exports/PurchaseOrderExport.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class PurchaseOrderExport implements FromCollection, WithHeadings, WithMapping
+{
+    protected $detail;
+
+    public function __construct($detail)
+    {
+        $this->detail = $detail;
+    }
+
+    public function collection()
+    {
+        return $this->detail;
+    }
+
+    public function headings(): array
+    {
+        return ['CANTIDAD', 'CÓDIGO', 'DESCRIPCIÓN']; // Aquí defines los encabezados de la hoja de Excel
+    }
+
+    public function map($order): array
+    {
+        $t = $order->product;
+        return [
+            $order->amount,
+            $order->product->provider_code,
+            $order->product->description,
+        ];
+    }
+}
--- app/Mail/PurchaseOrderEmail.php
+++ app/Mail/PurchaseOrderEmail.php
@@ -2,11 +2,13 @@
 
 namespace App\Mail;
 
+use App\Exports\PurchaseOrderExport;
 use App\Http\Resources\PurchaseOrder\PurchaseOrderResource;
 use App\Models\PurchaseOrder;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Maatwebsite\Excel\Facades\Excel;
 
 use App\Models\Shipment;
 
@@ -33,11 +35,17 @@ class PurchaseOrderEmail extends Mailable
     public function build()
     {
 
+        $filePath = "exports/order_de_compra_{$this->purchase_order->id}.xlsx";
+
+        // Guardar temporalmente el archivo en storage
+        Excel::store(new PurchaseOrderExport($this->purchase_order->detail), $filePath, 'local');
+
         return $this
             ->subject("Pedido {$this->purchase_order->provider->name}")
             ->view('emails.proveedor.orden_compra')
             ->with([
                 'oc' => new PurchaseOrderResource($this->purchase_order, 'complete'),
-            ]);
+            ])
+            ->attach(storage_path("app/{$filePath}"));;
     }
 }
